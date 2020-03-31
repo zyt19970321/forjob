ffbd02569d94e5405b2654e15ae60e7537de0395
张延涛 <zyt19970321@aliyun.com>
2020-03-31 11:37:26 +0800

    项目搭建

---
+++ src/main/java/com/zyt/forjob/ForjobApplication.java
@@ -0,0 +1,13 @@
+package com.zyt.forjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyt/forjob/ForjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyt.forjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
